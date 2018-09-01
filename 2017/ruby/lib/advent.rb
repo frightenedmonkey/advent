d47729beface594655f9e32e54f36865d0fe572f
Michael Lewis <frightenedmonkey@gmail.com>
2018-09-01 08:50:14 -0700

    Move specs over to captcha_spec
    Move all initial captcha logic over to the captcha.rb class & tests to
    captcha spec.

--- 2017/ruby/lib/advent.rb
+++ 2017/ruby/lib/advent.rb
@@ -1,34 +1,4 @@
 require 'advent/captcha'
 
 class Advent
-  def parse_captcha(digits)
-    splits = splitter(digits)
-    separated = sequentially_awesome(splits)
-    captcha_sum(separated)
-  end
-
-  def splitter(digits)
-    String(digits).split('').map {|i| Integer(i) }
-  end
-
-  def sequentially_awesome(digits)
-    collection = []
-    len = digits.length
-    digits.each_with_index do |val, idx|
-      if idx < (len - 1)
-        collection.push val if val == digits[idx + 1]
-      elsif idx == (len - 1)
-        collection.push val if val == digits[0]
-      end
-    end
-    collection
-  end
-
-  def captcha_sum(digits)
-    if digits.empty?
-      0
-    else
-      digits.sum
-    end
-  end
 end
---
+++ 2017/ruby/spec/captcha_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe Captcha do
+  subject { Captcha.new }
+  it 'should return 3 from 1122' do
+    expect(subject.parse_captcha(1122)).to eq(3)
+  end
+
+  it 'should return 4 from 1111' do
+    expect(subject.parse_captcha(1111)).to eq(4)
+  end
+
+  it 'should return 0 from 1234' do
+    expect(subject.parse_captcha(1234)).to eq(0)
+  end
+
+  it 'should return 9 from 91212129' do
+    expect(subject.parse_captcha(91212129)).to eq(9)
+  end
+
+  describe 'Advent#sequentially_awesome' do
+    it 'should return [] from 1234' do
+      expect(subject.sequentially_awesome([1,2,3,4])).to eq([])
+    end
+    it 'should return [1,1,2,2] from 1122' do
+      expect(subject.sequentially_awesome([1,1,2,2])).to eq([1,2])
+    end
+  end
+  describe 'Advent#captcha_sum' do
+    it 'should return 0 from []' do
+      expect(subject.captcha_sum([])).to eq(0)
+    end
+    it 'should return 3 from [1,2]' do
+      expect(subject.captcha_sum([1,2])).to eq(3)
+    end
+  end
+end
+
