bea313f74b9b61cc061d5bbff4a9402e34dd2ac1
Michael Lewis <frightenedmonkey@gmail.com>
2018-09-03 22:54:36 -0700

    Solutions to day 3's spiral challenge
    This was not my favorite & I churned on it quite a bit. I also wrote a
    ton of code that I trashed because I didn't need it. Ideally, I should
    have started with the logic of how to move a point on the board without
    really contemplating a way to avoid walking the spiral.

--- 2017/ruby/lib/advent/spiral.rb
+++ 2017/ruby/lib/advent/spiral.rb
@@ -1,24 +1,141 @@
-require 'math'
-
 module Advent
+  class Point
+    attr_accessor :x, :y, :counter
 
-  class Spiral
-    include Math
-    def self.generate! num
+    def initialize(x,y, counter = 1)
+      @x = x
+      @y = y
+      @counter = 1
     end
 
-    # to determine the size of the board, we need to find the square root of the
-    # largest int. The next largest odd integer is the width & height of the board.
-    # The square of that should tell us the largest integer on the board as well.
-    def size_board largest_int
-      floor = Integer(Math.sqrt(largest_int))
-      #return floor if ((floor * floor) == largest_int)
+    def manhattan_distance
+      x.abs + y.abs
+    end
 
-      if floor.even?
-        floor + 1
-      else
-        floor + 2
+    def current_position
+      [x, y]
+    end
+
+    def surrounding_coordinates
+      surrounding = []
+      [-1,1].each do |first_adder|
+        first_val = x + first_adder
+        surrounding.push [first_val, y]
+        [-1,1].each do |second_adder|
+          second_val = y + second_adder
+          surrounding.push [x, second_val]
+          surrounding.push [first_val, second_val]
+        end
       end
+      surrounding.sort.uniq
+    end
+
+    def corner?
+      x.abs == y.abs
+    end
+
+    def corner_position
+      return false if !corner?
+
+      if x.positive? && y.negative?
+        :bottom_right
+      elsif x.negative? && y.positive?
+        :top_left
+      elsif x.negative? && y.negative?
+        :bottom_left
+      elsif x.positive? && y.positive?
+        :top_right
+      end
+    end
+
+    def up
+      @y = @y + 1
+    end
+
+    def down
+      @y = @y - 1
+    end
+
+    def left
+      @x = @x - 1
+    end
+
+    def right
+      @x = @x + 1
+    end
+
+    def corner_move
+      case corner_position
+      when :top_right
+        :left
+      when :bottom_right
+        :right
+      when :top_left
+        :down
+      when :bottom_left
+        :right
+      end
+    end
+
+    def move
+      mv = next_move
+      send(mv)
+      @counter = @counter + 1
+    end
+
+    # max_axis is a positive integer that is equal to the largest absolute x or y
+    # value that can exist.
+    def next_move
+      if x == 0 && y ==0
+        return :right
+      end
+
+      if corner?
+        return corner_move
+      end
+
+      # order of the conditional here matters
+      if y.positive? && y >= x.abs
+        return :left
+      elsif x.negative? && x.abs > y.abs
+        return :down
+      elsif y.negative? && y.abs > x.abs
+        return :right
+      elsif x.positive? && x.abs > y
+        return :up
+      end
+    end
+  end
+
+  class Spiral
+    attr_accessor :point, :max_axis, :board_size
+
+    def walk_the_spiral target_int
+      @point = Advent::Point.new(0,0)
+
+      while @point.counter < target_int
+        @point.move
+      end
+
+      @point.manhattan_distance
+    end
+
+    def generate_crazy_board target_value
+      @point = Advent::Point.new(0,0)
+      board = {}
+      board[@point.current_position] = 1
+
+      last_value = 1
+      while last_value <= target_value
+        @point.move
+        surrounding_points = @point.surrounding_coordinates
+        new_point_value = surrounding_points.map do |coords|
+          board.fetch(coords, 0)
+        end.reduce(&:+)
+        board[@point.current_position] = new_point_value
+        last_value = new_point_value.dup
+      end
+      last_value
     end
   end
 end
--- 2017/ruby/spec/spiral_spec.rb
+++ 2017/ruby/spec/spiral_spec.rb
@@ -1,10 +1,161 @@
 require 'spec_helper'
 
 describe Advent::Spiral do
-  describe '#size_board' do
-    it 'should return the next odd integer up of the square root of the input' do
-      expect(subject.size_board(9)).to eq(3)
-      expect(subject.size_board(10)).to eq(5)
+  describe '#walk_the_spiral' do
+    it 'should be return the right manhattan distance' do
+      expect(subject.walk_the_spiral(11)).to eq(2)
+      expect(subject.walk_the_spiral(23)).to eq(2)
+      expect(subject.walk_the_spiral(21)).to eq(4)
+      expect(subject.walk_the_spiral(26)).to eq(5)
+      expect(subject.walk_the_spiral(347991)).to eq(480)
+    end
+  end
+  describe '#generate_crazy_board' do
+    it 'should be return the right manhattan distance' do
+      expect(subject.generate_crazy_board(11)).to eq(23)
+      expect(subject.generate_crazy_board(23)).to eq(25)
+      expect(subject.generate_crazy_board(54)).to eq(57)
+      expect(subject.generate_crazy_board(133)).to eq(142)
+      expect(subject.generate_crazy_board(347991)).to eq(349975)
+    end
+  end
+end
+
+describe Advent::Point do
+  subject { Advent::Point.new(1,-2) }
+  describe '#manhattan_distance' do
+    it 'should return the sum of the absolute value of the x,y' do
+      expect(subject.manhattan_distance).to eq(3)
+    end
+  end
+  describe '#current_position' do
+    it 'should return the x, y values in an array' do
+      expect(subject.current_position).to eq([1,-2])
+    end
+  end
+
+  describe '#corner?' do
+    context 'the abs value of the coordinates are equal' do
+      subject { Advent::Point.new(5, -5) }
+      it 'should return true' do
+        expect(subject.corner?).to be true
+      end
+    end
+    context 'the abs value of the coordinates are not equal' do
+      subject { Advent::Point.new(5, -4) }
+      it 'should return true' do
+        expect(subject.corner?).to be false
+      end
+    end
+  end
+
+  describe 'movements' do
+    subject { Advent::Point.new(1,-2) }
+    it 'should increment y when moving up' do
+      subject.up
+      expect(subject.current_position).to eq([1, -1])
+    end
+    it 'should decrement y when moving down' do
+      subject.down
+      expect(subject.current_position).to eq([1, -3])
+    end
+    it 'should increment x when moving right' do
+      subject.right
+      expect(subject.current_position).to eq([2, -2])
+    end
+    it 'should decrement x when moving left' do
+      subject.left
+      expect(subject.current_position).to eq([0, -2])
+    end
+  end
+
+  describe '#corner_position' do
+    context 'x == 2 && y == 2' do
+      subject { Advent::Point.new(2, 2) }
+      it 'should be :top_right' do
+        expect(subject.corner_position).to eq(:top_right)
+      end
+    end
+    context 'x == -2 && y == 2' do
+      subject { Advent::Point.new(-2, 2) }
+      it 'should be :top_left' do
+        expect(subject.corner_position).to eq(:top_left)
+      end
+    end
+    context 'x == -2 && y == -2' do
+      subject { Advent::Point.new(-2, -2) }
+      it 'should be :bottom_left' do
+        expect(subject.corner_position).to eq(:bottom_left)
+      end
+    end
+    context 'x == 2 && y == -2' do
+      subject { Advent::Point.new(2, -2) }
+      it 'should be :bottom_right' do
+        expect(subject.corner_position).to eq(:bottom_right)
+      end
+    end
+  end
+  describe '#next_move' do
+    subject { Advent::Point.new(2, 2) }
+    context 'not on a corner & the max_axis is 3' do
+      it 'should be :left' do
+        subject.x = 2
+        subject.y = 2
+        expect(subject.next_move).to eq(:left)
+      end
+      it 'should be :right' do
+        subject.x = -2
+        subject.y = -3
+        expect(subject.next_move).to eq(:right)
+      end
+      it 'should be :up' do
+        subject.x = 3
+        subject.y = 0
+        expect(subject.next_move).to eq(:up)
+      end
+      it 'should be :down' do
+        subject.x = -3
+        subject.y = 0
+        expect(subject.next_move).to eq(:down)
+      end
+    end
+    context 'on a corner & the max_axis is 3' do
+      it 'should be :left' do
+        subject.x = 3
+        subject.y = 3
+        expect(subject.next_move).to eq(:left)
+      end
+      it 'should be :right' do
+        subject.x = -3
+        subject.y = -3
+        expect(subject.next_move).to eq(:right)
+      end
+      it 'should be :right' do
+        subject.x = 3
+        subject.y = -3
+        expect(subject.next_move).to eq(:right)
+      end
+      it 'should be :down' do
+        subject.x = -3
+        subject.y = 3
+        expect(subject.next_move).to eq(:down)
+      end
+    end
+  end
+
+  describe '#surrounding_points' do
+    subject { Advent::Point.new(0, 0) }
+    it 'should return the surrounding coordinates' do
+      expect(subject.surrounding_coordinates).to eq([
+        [1, 0],
+        [1, 1],
+        [0, 1],
+        [-1,1],
+        [-1,0],
+        [-1,-1],
+        [0,-1],
+        [1,-1],
+      ].sort)
     end
   end
 end
