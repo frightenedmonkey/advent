6a9954733618ca4efc4e9d6dc3b1eb4759611091
Michael Lewis <frightenedmonkey@gmail.com>
2018-09-09 11:32:50 -0700

    Add the second solution (anagrams) for passphrase validity

--- 2017/ruby/spec/passphrase_spec.rb
+++ 2017/ruby/spec/passphrase_spec.rb
@@ -9,4 +9,23 @@ describe Advent::Passphrase do
   it 'should return false if a passphrase is invalid' do
     expect(subject.valid?('aa bb cc dd aa')).to be false
   end
+
+  it 'should return true if no phrase is an anagram' do
+    expect(subject.anagram_valid?('abcde fghij')).to be true
+    expect(subject.anagram_valid?('a ab abc abd abf abj')).to be true
+    expect(subject.anagram_valid?('iiii oiii ooii oooi oooo')).to be true
+  end
+
+  it 'should return false if any phrase is an anagram of another' do
+    expect(subject.anagram_valid?('abcde xyz ecdab')).to be false
+    expect(subject.anagram_valid?('oiii ioii iioi iiio')).to be false
+  end
+
+  describe '#anagram?' do
+    context 'oiii ioii' do
+      it 'should return true' do
+        expect(subject.anagram?('oiii', 'ioii')).to be true
+      end
+    end
+  end
 end
