06a7b0aa1bfb228fbd53d5219245059515ffccc3
Michael Lewis <frightenedmonkey@gmail.com>
2018-09-22 17:22:03 -0700

    Implement jump logic

--- 2017/ruby/lib/advent/jumps.rb
+++ 2017/ruby/lib/advent/jumps.rb
@@ -1,4 +1,62 @@
 module Advent
   class Jumps
+    attr_accessor :offsets, :current_location, :steps
+
+    # the offsets is an array of values; each value in the array shows what the
+    # offset for the particular array location (memory location) is.
+    def initialize(offsets)
+      @offsets = offsets
+      @current_location = 0 # always start at the 0 index?
+      @steps = 0
+      @escaped = false
+    end
+
+    def jump!
+      move = jump_offset.dup
+      next_location = @current_location + move
+      if next_location_valid?(next_location)
+        increment_current_offset
+        @current_location = next_location
+        # Now mutate the target location by incrementing by 1
+        increment_steps
+      else
+        # the next move will escape the maze, so increment the steps so we actually
+        # jump, and then set escaped to true
+        increment_steps
+        @escaped = true
+      end
+    end
+
+    # the location argument corresponds to an index location in the offsets array,
+    # so if the offsets array doesn't have that index, then it isn't a valid jump
+    # target.
+    def next_location_valid? location
+      # presumably, a negative location would be invalid
+      return false if location.negative?
+
+      # now check whether the location goes out beyond the array bounds in the
+      # positive direction
+      location < @offsets.length
+    end
+
+    # This will return the jump offset value for the current location, which is
+    # really just the value of the offset array that current index location
+    def jump_offset
+      @offsets[@current_location]
+    end
+
+    def increment_steps
+      @steps = @steps + 1
+    end
+
+    def move_until_exit
+      while !@escaped
+        jump!
+      end
+    end
+
+    def increment_current_offset
+      @offsets[@current_location] += 1
+    end
   end
 end
--- 2017/ruby/spec/jumps_spec.rb
+++ 2017/ruby/spec/jumps_spec.rb
@@ -1,7 +1,44 @@
 require 'spec_helper'
 
 describe Advent::Jumps do
+  subject { Advent::Jumps.new([0,1,2,3]) }
   it 'should initialize do' do
     expect(subject).to_not be_nil
+  end
+
+  describe '#jump!' do
+    it 'should increment an offset when it jumps from the location' do
+      subject.jump!
+      expect(subject.offsets).to eq([1,1,2,3])
+    end
+
+    it 'should add to the number of steps' do
+      subject.jump!
+      expect(subject.steps).to eq(1)
+      subject.jump!
+      expect(subject.steps).to eq(2)
+    end
+  end
+
+  describe '#next_location_valid?' do
+    context 'when the next location is a valid index location' do
+      it 'should return true' do
+        expect(subject.next_location_valid?(0)).to be true
+      end
+    end
+    context 'the next move exits the maze' do
+      subject { Advent::Jumps.new([20,0,2]) }
+      it 'should return false' do
+        expect(subject.next_location_valid?(20)).to be false
+      end
+    end
+  end
+
+  describe '#move_until_exit' do
+    subject { Advent::Jumps.new([0,3,0,1,-3]) }
+    it 'should take 5 steps to exit' do
+      subject.move_until_exit
+      expect(subject.steps).to eq 5
+    end
   end
 end
